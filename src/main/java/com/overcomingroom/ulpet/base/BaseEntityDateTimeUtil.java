0a00b3a5c73a5b1af8423b60dde2658ca025ef77
Shjung <wjdtngus9@gmail.com>
2024-06-18 17:02:37 +0900

    :sparkles: feat : 반려동물 동반 정보 api 호출 추가 + 메서드 분리 등으로 인한 코드 리팩 토링 진행

--- src/main/java/com/overcomingroom/ulpet/base/BaseEntityDateTimeUtil.java
+++ src/main/java/com/overcomingroom/ulpet/base/BaseEntityDateTimeUtil.java
@@ -2,6 +2,7 @@ package com.overcomingroom.ulpet.base;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -11,15 +12,33 @@ public class BaseEntityDateTimeUtil {
     /**
      * BaseEntity 의 속성에 맞게 시간을 변환합니다.
      *
-     * @param apiDateTime api에서 가져온 날짜
+     * @param apiDateTime     api에서 가져온 날짜
      * @param datetimePattern 변환할 대상의 패턴
      * @return LocalDateTime
      */
-    public static LocalDateTime localDateTimeParse(String apiDateTime, String datetimePattern){
+    public static LocalDateTime localDateTimeToLocalDateTimeParse(String apiDateTime, String datetimePattern) {
         // DateTimeFormatter를 사용하여 문자열의 포맷 지정
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datetimePattern);
 
         return LocalDateTime.parse(apiDateTime, formatter);
+    }
+
+    /**
+     * BaseEntity 의 속성에 맞게 시간을 변환합니다.
+     *
+     * @param apiDateTime     api에서 가져온 날짜
+     * @param datetimePattern 변환할 대상의 패턴
+     * @return LocalDateTime
+     */
+    public static LocalDateTime localDateToLocalDateTimeParse(String apiDateTime, String datetimePattern) {
+
+        // DateTimeFormatter를 사용하여 문자열의 포맷 지정
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datetimePattern);
+        // LocalDate로 변환
+        LocalDate localDate = LocalDate.parse(apiDateTime, formatter);
+
+        // LocalDateTime 변환, 00시 00분 00초
+        return localDate.atStartOfDay();
     }
 
 }
