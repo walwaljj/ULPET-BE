d910b4c28525c0b416ba044c80fe4783298dfb47
Shjung <wjdtngus9@gmail.com>
2024-06-21 18:22:33 +0900

    :sparkles: feat : api 호출 및 파싱 후 data를 저장하거나 변경을 감지하고 업데이트합니다.

--- src/main/java/com/overcomingroom/ulpet/place/repository/PlaceRepository.java
+++ src/main/java/com/overcomingroom/ulpet/place/repository/PlaceRepository.java
@@ -3,5 +3,10 @@ package com.overcomingroom.ulpet.place.repository;
 import com.overcomingroom.ulpet.place.domain.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+    Optional<Place> findByContentId(long contentId);
+
+    Optional<Place> findByPlaceNameAndAddress(String placeName, String address);
 }
