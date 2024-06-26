06cab8775477f7b513648ce9723f6a7ac1c82fcd
Shjung <wjdtngus9@gmail.com>
2024-06-27 03:31:21 +0900

    :recycle: refactor : MemberEntity 속성 email을 username 으로 통일

--- src/main/java/com/overcomingroom/ulpet/member/domain/entity/MemberEntity.java
+++ src/main/java/com/overcomingroom/ulpet/member/domain/entity/MemberEntity.java
@@ -3,19 +3,15 @@ package com.overcomingroom.ulpet.member.domain.entity;
 import com.overcomingroom.ulpet.base.BaseEntityMember;
 import com.overcomingroom.ulpet.place.domain.entity.Place;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -33,14 +29,12 @@ public class MemberEntity extends BaseEntityMember implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
-    private String username;
-  
     @Column(nullable = false)
-    private String email;
-  
+    private String username;
+
     @Column(nullable = false)
     private String password;
-    
+
     @Column(nullable = false)
     private String nickname;
 
@@ -50,40 +44,40 @@ public class MemberEntity extends BaseEntityMember implements UserDetails {
 
     @ManyToMany
     private List<Place> wishList = new ArrayList<>();
-  
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 
     @Override
     public String getPassword() {
-      return password;
+        return password;
     }
 
     @Override
     public String getUsername() {
-      return username;
+        return username;
     }
 
     public static MemberEntity of(String username, String password, String nickname, String profileImage) {
-      var member = new MemberEntity();
-      member.setUsername(username);
-      member.setPassword(password);
-      member.setNickname(nickname);
-      member.setProfileImage(profileImage);
-      return member;
+        var member = new MemberEntity();
+        member.setUsername(username);
+        member.setPassword(password);
+        member.setNickname(nickname);
+        member.setProfileImage(profileImage);
+        return member;
     }
 
     public static MemberEntity of(Long memberId, String username, String password, String nickname, String profileImage, Float familiarity) {
-      var member = new MemberEntity();
-      member.setMemberId(memberId);
-      member.setUsername(username);
-      member.setPassword(password);
-      member.setNickname(nickname);
-      member.setProfileImage(profileImage);
-      member.setFamiliarity(familiarity);
-      return member;
+        var member = new MemberEntity();
+        member.setMemberId(memberId);
+        member.setUsername(username);
+        member.setPassword(password);
+        member.setNickname(nickname);
+        member.setProfileImage(profileImage);
+        member.setFamiliarity(familiarity);
+        return member;
     }
 
 }
